93da85563e6036d86742adf5870f6855150ce512
yashraj1906 <160738766+yashraj1906@users.noreply.github.com>
2024-02-26 17:02:31 +0530

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+    printf("Enter the array elements :");
+    int a[n];                    //Array Declaration
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)         //Sorting Array
+    {
+        int temp;
+        for(int j=i+1; j<n ;j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("The second largest element is %d",a[1]);      //Accessing the largest element
+    printf("\n");
+    return 0;
+}
